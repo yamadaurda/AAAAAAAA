f3e4da4b7e45a22589dc965f6d3bcbfee220654c
uwaaaaaaaaa <joule4273@gmail.com>
2019-09-27 20:59:20 +0900

    Real info

--- Azechi.cs
+++ Azechi.cs
@@ -2,7 +2,7 @@ public class Azechi{
     public string name;
     public int age;
     public Azechi(){
-        name = "ひも"；
-        age = 10;
+        name = "あぜっちゃん"；
+        age = 21;
     }
 }
